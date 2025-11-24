109f635803ae69339131904fa20ea2fb4f22449b
KryptosNite <jordan.pia2007@gmail.com>
2025-11-24 19:00:27 +1000

    Delete supabase.js

--- supabase.js
+++
@@ -1,7 +0,0 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-export const supabase = createClient(
-    'https://ldnkttpcebdneoyhbkqy.supabase.co',
-    'sb_publishable_Pvr39hkOCE_T7Fdsy5UgCQ_PXFlIB_Y'
-);
